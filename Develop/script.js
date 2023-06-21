53c4eab28f8db7b03e46cea76cdb66e1c46f6ae0
palmtopj <palmtopj@uw.edu>
2023-06-20 20:24:39 -0700

    added page clock

--- Develop/script.js
+++ Develop/script.js
@@ -2,6 +2,11 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  $(".saveBtn").on("click", function () {
+    var timeBlock = $(this).closest("time-block").attr("id");
+    var userText = $(this).siblings(".description").val();
+    localStorage.setItem(timeBlock, userText);
+  });
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +25,6 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  var currentDate = dayjs().format('MMMM D, YYYY');
+  $('#currentDay').text(currentDate);
 });
